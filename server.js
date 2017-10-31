a65f6907eba11b49a9cc7503e39a4f410c377e02
Robin Millette <robin@millette.info>
2017-10-31 07:46:33 -0400

    Server now uses lru cache.

--- server.js
+++ server.js
@@ -3,17 +3,32 @@
 // npm
 const micro = require('micro')
 const parse = require('urlencoded-body-parser')
+const AsyncLRU = require('async-lru')
+
+// core
+const { promisify } = require('util')
 
 // self
 const isKnownPassword = require('.')
 
+const max = 100000
+const port = 3050
+
+const lru = new AsyncLRU({
+  max,
+  load: (pw, cb) => isKnownPassword(pw)
+    .then((x) => cb(null, x))
+    .catch(cb)
+})
+
 const throwError = (code, str) => { throw micro.createError(code || 500, str || 'Error') }
+const testPassword = promisify(lru.get.bind(lru))
 const server = micro(async (req) => req.method.toUpperCase() === 'POST'
   ? parse(req)
     .then((x) => (x && x.password) ? x.password : throwError(400, 'Need password field.'))
-    .then(isKnownPassword)
+    .then(testPassword)
     .then((ok) => ok ? { ok, msg: 'OK to use' } : { ok, msg: 'NOT ok to use' })
   : throwError(405, 'Method not allowed. Only POST is supported.')
 )
 
-server.listen(3050)
+server.listen(port)
