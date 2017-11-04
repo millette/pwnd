175c80a196d0e60bc45e9ca2ebade677516279c9
Robin Millette <robin@millette.info>
2017-11-04 09:01:40 -0400

    Check free space (20 GiB required).

--- scripts/init.js
+++ scripts/init.js
@@ -1,11 +1,24 @@
 'use strict'
 
+// npm
+const disk = require('diskusage')
+
 // self
 const utils = require('../utils')
 
-utils.which7z()
-  .then((bin) => Promise.all([bin, utils.check(bin, utils.pwndFiles.files[2])]))
-  .then(([bin]) => Promise.all([bin, utils.check(bin, utils.pwndFiles.files[1])]))
-  .then(([bin]) => utils.check(bin, utils.pwndFiles.files[0]))
-  .then(console.log)
-  .catch(console.error)
+console.log('It takes a few minutes to download, verify and decompress 13 GiB. Please be patient.')
+
+disk.check('.', (err, info) => {
+  if (err) { return console.error(err) }
+  if (info.available < 20e9) {
+    console.log('You need at least 20 GiB free to download and decompress the files.')
+    return
+  }
+
+  utils.which7z()
+    .then((bin) => Promise.all([bin, utils.check(bin, utils.pwndFiles.files[2])]))
+    .then(([bin]) => Promise.all([bin, utils.check(bin, utils.pwndFiles.files[1])]))
+    .then(([bin]) => utils.check(bin, utils.pwndFiles.files[0]))
+    .then(console.log)
+    .catch(console.error)
+})
