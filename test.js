ab4f2ce32b721e65c9c24a61cac5a443067df8d4
Robin Millette <robin@millette.info>
2017-11-04 11:48:47 -0400

    Add test.

--- test.js
+++ test.js
@@ -5,6 +5,7 @@ import test from 'ava'
 
 // self
 import fn from '.'
+import utils from './utils'
 
 test('password, small file', async t => {
   const ret = await fn('password', 1)
@@ -29,4 +30,8 @@ test.skip('passétéword, all files', async t => {
 test('hashed, small file', async t => {
   const ret = await fn('0000C1BDAB3A615C3B636084CC33171482948D5A', 1)
   t.is(ret, false)
+})
+
+test('wrk', t => {
+  t.is(utils.wrkName({ url: 'bob.7z' }), 'wrk/bob')
 })
